ad5f447923f5ce53cabf7386e513fb5ba0c3f834
gdprasad1201 <gdprasad@crimson.ua.edu>
2024-12-31 22:09:36 -0500

    first commit

---
+++ src/main/java/com/FreeMess/JavaMessagePlatform/JavaMessagePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.FreeMess.JavaMessagePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMessagePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMessagePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FreeMess/JavaMessagePlatform/JavaMessagePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FreeMess.JavaMessagePlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessagePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
